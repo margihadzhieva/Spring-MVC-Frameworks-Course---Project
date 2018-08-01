31f8b8551d082a0cb1a822e259e820120b2190f3
Margi Hadzhieva <margarita.hadjieva@mail.bg>
2018-08-01 21:19:17 +0300

    navigation bar for index Page

---
+++ cinema-system/src/main/org.softuni.cinemasystem/CinemaSystemApplication.java
@@ -0,0 +1,13 @@
+package org.softuni.cinemasystem;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+
+@SpringBootApplication
+public class CinemaSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaSystemApplication.class, args);
+    }
+}
