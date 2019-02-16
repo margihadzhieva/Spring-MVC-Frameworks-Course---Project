5d907fded3e44e3f0fde11eec77acca1336cca82
Margarita Hadjieva <margarita.hadjieva@mail.bg>
2019-02-16 21:10:27 +0200

    modal for adding halls

--- cinema-system/src/main/org.softuni.cinemasystem/controllers/CinemaController.java
+++ cinema-system/src/main/org.softuni.cinemasystem/controllers/CinemaController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -55,11 +56,11 @@ public class CinemaController extends BaseController {
 
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public String addCinema(CinemaCreateBindingModel cinemaCreateBindingModel, BindingResult bindingResult, Model model) {
+    public ModelAndView addCinema(@ModelAttribute  CinemaCreateBindingModel cinemaCreateBindingModel) {
         this.cinemaService.addCinema(this.modelMapper
                 .map(cinemaCreateBindingModel
                         , CinemaServiceModel.class));
-        return "redirect:/admin";
+        return this.redirect("all");
     }
 
 
